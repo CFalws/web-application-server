9302c0b61d56f763b4d22b98498ce6de08814cfa
dhkim <akdlzmf1123@gmail.com>
2022-10-19 20:46:59 +0900

    refactor: 클래스 분리

--- src/main/java/model/ResponseRenderer.java
+++ src/main/java/model/ResponseRenderer.java
@@ -1,12 +1,19 @@
 package model;
 
+import db.DataBase;
+import util.HttpRequestParser;
 import webserver.RequestHandler;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class ResponseRenderer {
+    private static final String DEFAULT_PATH = "/index.html";
+    private static final String HOST = "http://localhost:8080";
+    private static final String WEBAPP_PATH = "./webapp";
     private BufferedReader br;
     private DataOutputStream dos;
     public ResponseRenderer(BufferedReader br, DataOutputStream dos) {
@@ -14,7 +21,76 @@ public class ResponseRenderer {
         this.dos = dos;
     }
 
-    public void render(RequestHandler handler) throws IOException {
-        handler.makeResp(br, dos);
+    public void render() throws IOException {
+        String path = HttpRequestParser.requestLine(br);
+        switch (path) {
+            case "/user/create":
+                UserManager.create(br);
+                resp302(HOST + DEFAULT_PATH);
+                break;
+            case "/user/login":
+                if (UserManager.signIn(br)) resp302SignInSuccess(HOST + DEFAULT_PATH);
+                else resp302SignInFail(HOST + "/user/login_failed.html");
+                break;
+            case "/user/list":
+                if (HttpRequestParser.isSignedIn(br)) resp200AllUser();
+                else resp302(HOST + "/user/login.html");
+                break;
+            default:
+                resp200(path);
+                break;
+        }
+    }
+
+    private void resp302SignInSuccess(String location) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Set-Cookie: logined=true\r\n");
+        dos.writeBytes("Location: " + location + "\r\n");
+    }
+
+    private void resp302SignInFail(String location) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Set-Cookie: logined=false\r\n");
+        dos.writeBytes("Location: " + location + "\r\n");
+    }
+
+    private void resp302(String location) throws IOException {
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Location: " + location + "\r\n");
+    }
+
+    private void resp200AllUser() throws IOException {
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        byte[] body = DataBase.findAll().toString().getBytes();
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        dos.writeBytes("\r\n");
+        responseBody(body);
+    }
+
+
+    private void resp200(String path) throws IOException {
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        byte[] body = getBytes(path);
+        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        dos.writeBytes("\r\n");
+        responseBody(body);
+    }
+
+    private byte[] getBytes(String path) {
+        if (path.equals("/")) {
+            return "hello world".getBytes();
+        }
+        try {
+            return Files.readAllBytes(new File(WEBAPP_PATH + path).toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void responseBody(byte[] body) throws IOException {
+        dos.write(body, 0, body.length);
+        dos.flush();
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -14,9 +14,7 @@ import util.HttpRequestParser;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-    private static final String DEFAULT_PATH = "/index.html";
-    private static final String HOST = "http://localhost:8080";
-    public static final String WEBAPP_PATH = "./webapp";
+
 
     private Socket connection;
 
@@ -31,82 +29,9 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             new ResponseRenderer(new BufferedReader(new InputStreamReader(in))
-                    , new DataOutputStream(out)).render(this);
+                    , new DataOutputStream(out)).render();
         } catch (IOException e) {
             log.error(e.getMessage());
         }
-    }
-
-    public void makeResp(BufferedReader br, DataOutputStream dos) throws IOException {
-        String path = HttpRequestParser.requestLine(br);
-        switch (path) {
-            case "/user/create":
-                UserManager.create(br);
-                resp302(HOST + DEFAULT_PATH, dos);
-                break;
-            case "/user/login":
-                if (UserManager.signIn(br)) resp302SignInSuccess(HOST + DEFAULT_PATH, dos);
-                else resp302SignInFail(HOST + "/user/login_failed.html", dos);
-                break;
-            case "/user/list":
-                if (HttpRequestParser.isSignedIn(br)) resp200AllUser(dos);
-                else resp302(HOST + "/user/login.html", dos);
-                break;
-            default:
-                resp200(path, 200, dos, "");
-                break;
-        }
-    }
-
-    private void resp302SignInSuccess(String location, DataOutputStream dos) throws IOException {
-        dos.writeBytes("HTTP/1.1 302 Found \r\n");
-        dos.writeBytes("Set-Cookie: logined=true\r\n");
-        dos.writeBytes("Location: " + location + "\r\n");
-    }
-
-    private void resp302SignInFail(String location, DataOutputStream dos) throws IOException {
-        dos.writeBytes("HTTP/1.1 302 Found \r\n");
-        dos.writeBytes("Set-Cookie: logined=false\r\n");
-        dos.writeBytes("Location: " + location + "\r\n");
-    }
-
-    private void resp302(String location, DataOutputStream dos) throws IOException {
-        dos.writeBytes("HTTP/1.1 302 Found \r\n");
-        dos.writeBytes("Location: " + location + "\r\n");
-    }
-
-    private void resp200AllUser(DataOutputStream dos) throws IOException {
-        dos.writeBytes("HTTP/1.1 200 OK \r\n");
-        byte[] body = DataBase.findAll().toString().getBytes();
-        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        dos.writeBytes("Content-Length: " + body.length + "\r\n");
-        dos.writeBytes("\r\n");
-        responseBody(dos, body);
-    }
-
-
-    private void resp200(String path, int statusCode, DataOutputStream dos, String header) throws IOException {
-        dos.writeBytes("HTTP/1.1 200 OK \r\n");
-        byte[] body = getBytes(path);
-        dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-        dos.writeBytes("Content-Length: " + body.length + "\r\n");
-        dos.writeBytes("\r\n");
-        responseBody(dos, body);
-    }
-
-    private byte[] getBytes(String path) {
-        if (path.equals("/")) {
-            return "hello world".getBytes();
-        }
-        try {
-            return Files.readAllBytes(new File(WEBAPP_PATH + path).toPath());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) throws IOException {
-         dos.write(body, 0, body.length);
-         dos.flush();
     }
 }
